ab5e328783e35dc731ac824d152b986ea3937c46
yano <yano@liberogic.jp>
2020-05-18 16:06:00 +0900

    テスト

--- src/components/form/FileForm.vue
+++ src/components/form/FileForm.vue
@@ -2,7 +2,7 @@
   v-row(align="center" justify="center")
     v-col
       v-form(ref="form")
-        v-file-input(accept="text/plain" placeholder="Pick an text file" prepend-icon="mdi-paperclip" label="TextFile" show-size)
+        v-file-input(accept="text/plain" placeholder="Pick an text file" prepend-icon="mdi-paperclip" label="TextFile" v-on:change="change" show-size)
         v-btn(class="ma-2" color="indigo" outlined v-on:click="translate") Translate
         v-btn(class="ma-2" color="red" outlined v-on:click="reset()") Reset
 </template>
@@ -17,8 +17,11 @@ export default Vue.extend({
     }
   },
   methods: {
-    translate(e: any): void {
+    change(e: any): void {
       console.log(e)
+    },
+    translate(e: any): void {
+      console.log(this.$refs.form)
     },
     reset(): void {}
   }
