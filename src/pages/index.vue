b5c21b3d61ef864ff56d73f8cba582336802cefd
yano <yano@liberogic.jp>
2020-05-18 09:42:08 +0900

    不足パッケージ追加。 テンプレートをpugに統一。

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,12 @@
-<template>
-  <div class="container">
-    <div>
-      <!-- <logo /> -->
-      <Form />
-      <h1 class="title">
-        app
-      </h1>
-      <h2 class="subtitle">
-        My magnificent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  div(class="container")
+    div
+      Form
+      h1(class="title") app
+      h2(class="subtitle") My magnificent Nuxt.js project
+      div(class="links")
+        a(href="https://nuxtjs.org/" target="_blank" class="button--green") Documentation
+        a(href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey") Github
 </template>
 
 <script lang="ts">
